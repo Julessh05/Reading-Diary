54301630cba83dab159fd39d3a33920e2bafb7f3
Julian Schumacher <Jules.media@outlook.de>
2022-07-02 12:41:37 +0200

    Update final var to const

--- lib/models/book_list.dart
+++ lib/models/book_list.dart
@@ -6,17 +6,19 @@ import 'package:reading_diary/models/book.dart' show Book;
 import 'package:reading_diary/storage/storage.dart';
 
 class BookList {
-  static final List<Book> _books = [];
+  static const List<Book> _books = [];
 
   /// List of all Books the User stored.
   static List<Book> get books => UnmodifiableListView(_books);
 
-  /// Add a Book
+  /// Adds a specified [book]
   static void addBook(Book book) {
     _books.add(book);
     Storage.storeBooks();
   }
 
+  /// Deletes the specified [book]
+  /// from the List of Books
   static void deleteBook(Book book) {
     _books.remove(book);
     Storage.storeBooks();
